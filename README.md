defa2b2f85e2d76c918dd9a0dca5a323d7ef9b5e
Vince-Colson-TM <vincecolson@thomasmore.be>
2024-02-23 17:47:44 +0100

    Javascript exercises - Basics

---
+++ README.md
@@ -0,0 +1,10 @@
+# JavaScript Exercises
+
+## Overview
+In this repository, you'll find exercises designed to test and improve your JavaScript skills. 
+To maximize learning, try to solve the problems on your own and avoid looking up answers.
+
+Use efficient coding practices like arrow functions and ternary operators where applicable.
+
+## Good Luck!
+Remember, the goal is to learn and improve. Have fun!
---
+++ basics.js
@@ -0,0 +1,73 @@
+/*
+Javascript Exercises - Basics
+-----------------------------
+In this file you can find some exercises to put your Javascript skills to the test.
+These exercises are based on the 'basic' chapters in our course.
+Try to avoid the temptation of looking up the answer to maximize your learning.
+
+Imaginary bonus points for using more efficient coding practices like arrow functions and ternary operators where applicable.
+
+GOOD LUCK!
+*/
+
+
+/* Exercise 1
+-----------------------------
+Write a function that checks two numbers, return true if one of the numbers is 10 OR if the sum of the two numbers is 20, otherwise return false.
+*/
+
+
+/* Exercise 2
+-----------------------------
+Write a function that returns the file extension of a given filename (string).
+
+For example 'index.html' returns '.html'.
+*/
+
+
+/* Exercise 3
+-----------------------------
+Write a function that capitalizes a string and adds an exclamation mark (!) at the end.
+Only add the exclamation mark if it is not already present at the end of the string.
+
+For example 'test' returns 'TEST!'
+*/
+
+
+/* Exercise 4
+-----------------------------
+Write a function that takes a string as an argument, take the first 3 and the last 3 characters of this string and add them together.
+The length of the string needs to be 7 or more, if not send a message to the console.
+
+For example 'Test this function' returns 'Tesion'
+*/
+
+
+/* Exercise 5
+-----------------------------
+Write a function that compares two numbers and returns the value that is closest to 100.
+Make sure this also works when passing negative numbers or numbers higher than 100.
+*/
+
+
+/* Exercise 6
+-----------------------------
+Write a function that multiplies a given number by the value of Pi.
+Have a default value of 1 for the number.
+Round the result to the nearest integer.
+Log the result to the console with the message '... multiplied by Pi and rounded = ...'.
+*/
+
+
+/* Exercise 7
+-----------------------------
+Write a function that checks if an array of numbers is sorted in ascending order.
+*/
+
+
+
+/* Exercise 8
+-----------------------------
+Write a function that performs a basic arithmetic operation on two numbers.
+The function should accept three arguments: two numbers and a string representing the operation ('+', '-', '*', '/').
+*/
